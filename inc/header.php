76d53f62ee22d1d9576ee829c50e4fe4cad4f39b
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-10-24 11:52:52 +0200

    start

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <title>header</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="../assets/svg/logo.svg">
+        <div class="icons">
+            <img class="user" src="../assets/svg/user.svg">
+            <img class="menu" src="../assets/svg/menu.svg">
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once '../Trombinoscope/inc/header.php';
+?>
+
+
+
+
+
+
+
+<?php
+    require_once '../Trombinoscope/inc/footer.php';
+?>
