dde79fa6e1386ea33bbfb8dc63bbeab06ece66fb
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-10-31 15:50:50 +0100

    arborescence

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/reset.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/output.css">
     <title>header</title>
 </head>
 <body>
