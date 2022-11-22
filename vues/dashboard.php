df70d2c569fabc37c9315471230d42616791faa2
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-11-22 22:44:48 +0100

    end

--- vues/dashboard.php
+++ vues/dashboard.php
@@ -31,7 +31,6 @@ if(!is_not_empty_and_defined($_POST)) {
 <div class="users_list">
     <?php foreach(get_users_by_grade($_POST['grade']) as $user): ?>
         <div class="user">
-            <?= get_image_by_id($user['user_id'])[0]['img_bin'] ?>
             <img src="data:image/jpeg;base64,<?=get_image_by_id($user['user_id'])[0]['img_bin'] ?>" class="user_img">
             <h2 class="user_names"><?= $user['first_name'].' '.$user['last_name'] ?></h2>
             <form class="user_form" action="../vues/user_details.php" method="post">
